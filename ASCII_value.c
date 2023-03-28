62e75581baffe5103f302ae5f97277aea6c07c20
Adarsh <adarsh.lol2425@gmail.com>
2023-03-28 21:24:52 +0530

    third

---
+++ ASCII_value.c
@@ -0,0 +1,14 @@
+//ASCII value of Character
+
+#include<stdio.h>
+
+int main(){
+
+    char ch;
+
+    printf("Enter Character : ");
+    scanf("%c", &ch);
+
+    printf("\nASCII Value of %c is %d", ch, ch);
+    return 0;
+}
