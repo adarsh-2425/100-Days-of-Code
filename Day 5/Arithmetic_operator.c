707ff830fd14c32a9aab185485d57141552253b0
Adarsh <adarsh.lol2425@gmail.com>
2023-03-30 20:29:44 +0530

    arithmetic

---
+++ Day 5/Arithmetic_operator.c
@@ -0,0 +1,17 @@
+//Arithmetic operator
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 7;
+    
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a % b = %d\n", a % b);
+    
+
+    return 0;
+}
