fe2b5bb63a34b057c5d8b5a728dedcf957fa6bbb
Adarsh <adarsh.lol2425@gmail.com>
2023-03-31 10:01:20 +0530

    operator

--- Operators.c
+++ Operators.c
@@ -28,3 +28,25 @@ int main()
 
     return 0;
 }
+
+//Logical AND and OR operator
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 1, b = 6;
+    
+    int result1 = a-- && ++b;
+     printf("%d %d\n", a, b); // 0 7
+    int result2 = --a && ++b;
+     printf("%d %d\n", a, b); // -1 8
+    
+   
+    printf("result 1 is %d\n", result1); // 1
+    
+   
+    printf("result 1 is %d\n", result2); // 1
+
+    return 0;
+}
