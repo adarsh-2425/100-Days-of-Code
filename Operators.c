29c24b2003eeb8da8f8fbf3c9d78127b37ee56a8
Adarsh <adarsh.lol2425@gmail.com>
2023-03-31 09:36:29 +0530

    operator

---
+++ Operators.c
@@ -0,0 +1,12 @@
+//Logical operator
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 20;
+    int result = (a < b) && (printf("Logical And"));
+    printf("%d", result);
+
+    return 0;
+}
