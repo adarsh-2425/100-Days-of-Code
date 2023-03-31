66cc472ae5258eb055182f8cb5f1e5a880131519
Adarsh <adarsh.lol2425@gmail.com>
2023-03-31 09:43:08 +0530

    operator

--- Operators.c
+++ Operators.c
@@ -1,4 +1,4 @@
-//Logical operator
+//Logical AND operator
 
 #include <stdio.h>
 
@@ -6,7 +6,25 @@ int main()
 {
     int a = 10, b = 20;
     int result = (a < b) && (printf("Logical And"));
+
     printf("%d", result);
+    //result is Logical And 1
+
+    return 0;
+}
+
+//Logical OR operator
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 4, b = 6;
+    int result = (a > b) && (printf("Jenny")) || printf("lectures") && printf("jk");
+
+    printf("%d", result);
+    //result = lectures jk 1
+
 
     return 0;
 }
