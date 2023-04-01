169be5664902e11e268c03c5c5d267c3313cc4fa
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-01 17:57:41 +0530

    Update loops.c

--- loops.c
+++ loops.c
@@ -47,3 +47,18 @@ int main()
     return 0;
 }
 
+//print 5 stars using nested for loop
+
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
