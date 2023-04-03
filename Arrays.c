2162dca64422a9ced0cfed092c148898299c4c21
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-03 09:53:51 +0530

    Update Arrays.c

--- Arrays.c
+++ Arrays.c
@@ -26,3 +26,35 @@ int main()
 
     return 0;
 }
+
+// Program to get marks of 5 students and find sum and average
+
+#include <stdio.h>
+#define N 5 //defining value of N as 5
+
+int main()
+{
+    //initialization
+    int marks[N], sum = 0, i;
+    float avg;
+    
+    // Getting marks
+    printf("\nEnter Marks of 5 students : ");
+    for(i = 0;i < N; i++){
+        scanf("%d", &marks[i]);
+    }
+    
+    //calculating sum
+    for(i = 0;i < N; i++){
+        sum = sum + marks[i];
+        
+    }
+    
+    //calculating average
+    avg = sum / 2;
+    
+    printf("Sum of Marks of student are : %d\n", sum);
+    printf("Average Marks of student are : %f\n", avg);
+
+    return 0;
+}
