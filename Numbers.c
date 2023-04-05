c0a8958bf260c9af4a6f0bf865a077f024e7aea2
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-05 19:18:16 +0530

    Update Numbers.c

--- Numbers.c
+++ Numbers.c
@@ -21,3 +21,31 @@ int main()
     
     printf("Average of %d numbers is %f", n, avg);
 }
+
+//C Program to Check Armstrong Number
+
+#include <stdio.h>
+
+int main()
+{
+    int num, original, rem, result =0;
+    
+    printf("Enter Three Digit Number : ");
+    scanf("%d", &num);
+    
+    original = num;
+    
+    while(num != 0){
+        
+        rem = num % 10; //Remainder contains last digit of number
+        result += rem * rem * rem;
+        num /= 10; //Removes last digit
+    }
+    
+    if(result == original){
+        printf("%d is a Armstrong  Number", original);
+    }
+    else{
+         printf("%d is not a Armstrong  Number", original);
+    }
+}
