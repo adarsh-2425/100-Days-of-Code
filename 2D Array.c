184c481ba008beb9d52af924def7e019b87704a6
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-05 13:49:36 +0530

    Create 2D Array.c

---
+++ 2D Array.c
@@ -0,0 +1,33 @@
+//Program to print matrix and calculate sum
+
+#include <stdio.h>
+
+int main()
+{
+    int row = 2, col =3, i, j;
+    int mat1[row][col], sum = 0;
+    
+    printf("Enter Matrix : ");
+    for(i = 0; i < row; i++){
+        for(j = 0; j < col; j++){
+            
+            scanf("%d", &mat1[i][j]);
+            
+        }
+    }
+    
+    for(i = 0; i < row; i++){
+        for(j = 0; j < col; j++){
+            
+            printf("%d \n", mat1[i][j]);
+            
+            sum = sum + mat1[i][j];
+        }
+    }
+    
+    printf("Sum of matrix is : %d", sum);
+ 
+    
+
+    return 0;
+}
