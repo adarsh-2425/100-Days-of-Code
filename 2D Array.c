62a166e060488f4aa2e71ba9e738abe38b4f6a2d
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-05 14:46:38 +0530

    Update 2D Array.c

--- 2D Array.c
+++ 2D Array.c
@@ -31,3 +31,49 @@ int main()
  
     return 0;
 }
+
+//Program to print transpose of a matrix
+
+#include <stdio.h>
+
+int main()
+{
+    int row = 2, col =3, i, j;
+    int mat[row][col], transpose[row][col];
+    
+    //Getting Matrix
+    printf("Enter Matrix : ");
+    for(i = 0; i < row; i++){
+        for(j = 0; j < col; j++){
+            scanf("%d", &mat[i][j]);
+        }
+    }
+    
+    // Transpose of Matrix
+    for(i = 0; i < col; i++){
+        for(j = 0; j < row; j++){
+            transpose[i][j] = mat[j][i];
+        }
+        printf("\n");
+    }
+    
+    //Printing Matrix 1
+    printf("\nMatrix is\n");
+     for(i = 0; i < row; i++){
+        for(j = 0; j < col; j++){
+            printf("%d\t", mat[i][j]);
+        }
+        printf("\n");
+    }
+    
+    //Printing Transpose of Matrix
+    printf("\nTranspose of Matrix is\n");
+     for(i = 0; i < col; i++){
+        for(j = 0; j < row; j++){
+            printf("%d\t", transpose[i][j]);
+        }
+        printf("\n");
+    }
+ 
+    return 0;
+}
