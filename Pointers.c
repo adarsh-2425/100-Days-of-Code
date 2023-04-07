861974879c42cccb919f62c9d451515c5d1ee70c
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-07 19:13:45 +0530

    Create Pointers.c

---
+++ Pointers.c
@@ -0,0 +1,14 @@
+//Address of(&) and Indirection (*) operator in Pointers
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int a = 5;
+    int *p = &a;
+    
+    printf("%d", *p);
+
+    return 0;
+}
