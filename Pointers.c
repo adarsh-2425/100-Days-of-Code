fdb726e231ca5fd41fd5dce8595f86496ed97f0e
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-08 12:33:07 +0530

    Update Pointers.c

--- Pointers.c
+++ Pointers.c
@@ -12,3 +12,19 @@ int main()
 
     return 0;
 }
+
+//Pointer Addition
+
+#include <stdio.h>
+
+int main()
+{
+    int a[5] = {0, 1, -1, 10, 11};
+    int *p = &a[0];
+    
+    p  = p + 2; // p + n * sizeofdatatype
+    
+    printf("%d", *p);
+
+    return 0;
+}
