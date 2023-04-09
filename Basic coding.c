ae9e8888f12f056afade687f1f69f2c6a7034448
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-09 11:00:13 +0530

    Update Basic coding.c

--- Basic coding.c
+++ Basic coding.c
@@ -332,3 +332,27 @@ int main()
     
     return 0;
 }       
+
+       
+       
+//Swapping Two Numbers
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    
+    printf("Enter Numbers : ");
+    scanf("%d %d", &a, &b);
+    
+    printf("\nBefore Swapping : a = %d, b = %d", a, b);
+    
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+     printf("\nAfter Swapping : a = %d, b = %d", a, b);
+    
+    return 0;
+}
