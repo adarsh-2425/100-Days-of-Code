ccb943c848d3d96fc64e4766d8dde8cb57bc0f78
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-11 12:34:53 +0530

    Update Programming Interview Questions.c

--- Programming Interview Questions.c
+++ Programming Interview Questions.c
@@ -222,3 +222,35 @@ void lowercase(char str[])
 }
 
 
+// How do you get the matching elements in an integer array
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+   int a[10];
+   int i, j;
+   
+   printf("Enter Elements : ");
+   for(i = 0; i < 5; i++)
+   {
+       scanf("%d", &a[i]);
+   }
+   
+   for(i = 0; i < 5; i++)
+   {
+       for(j = i +1; j < 5; j++)
+       {
+           if(a[i] == a[j])
+           {
+               printf("%d ", a[i]);
+           }
+       }
+   }
+    
+    return 0;
+    
+}
+
