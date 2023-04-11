fd6b2d05d2e1ba090057aa5a17f16efc698c53c5
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-11 13:03:33 +0530

    Update Programming Interview Questions.c

--- Programming Interview Questions.c
+++ Programming Interview Questions.c
@@ -307,3 +307,52 @@ int main()
 }
 
 
+//  How would you implement the insertion sort algorithm?
+
+#include <stdio.h>
+
+
+
+int main()
+{
+   int a[10];
+   int i, j, temp, flag;
+   
+   printf("Enter Elements : ");
+   for(i = 0; i < 5; i++)
+   {
+       scanf("%d", &a[i]);
+   }
+   
+   printf("Before Sorting : ");
+   for(i = 0; i < 5; i++)
+   {
+       printf("%d ",a[i]);
+   }
+   
+   //Insertion sort
+   for(i = 1; i < 5; i++)
+   {
+        temp = a[i];
+        j = i - 1;
+       
+       while (j >= 0 && a[j] > temp)
+       {
+           a[j+1] = a[j];
+           j--;
+       }
+       
+      a[j+1] = temp;
+   }
+   
+   printf("\nAfter Sorting : ");
+   for(i = 0; i < 5; i++)
+   {
+       printf("%d ",a[i]);
+   }
+    
+    return 0;
+    
+}
+
+
