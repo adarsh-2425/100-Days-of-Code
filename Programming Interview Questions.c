18598b064a6624dc091cd04ce74bb88d09cfb2f7
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-11 12:46:05 +0530

    Update Programming Interview Questions.c

--- Programming Interview Questions.c
+++ Programming Interview Questions.c
@@ -254,3 +254,56 @@ int main()
     
 }
 
+
+// How would you implement the bubble sort algorithm?
+
+#include <stdio.h>
+
+
+
+int main()
+{
+   int a[10];
+   int i, j, temp, flag;
+   
+   printf("Enter Elements : ");
+   for(i = 0; i < 5; i++)
+   {
+       scanf("%d", &a[i]);
+   }
+   
+   printf("Before Sorting : ");
+   for(i = 0; i < 5; i++)
+   {
+       printf("%d ",a[i]);
+   }
+   
+   //bubble sort
+   for(i = 0; i < 5-1; i++)
+   {
+       flag = 0;
+       for(j = 0; j < 5-1-i; j++)
+       {
+           if(a[j] > a[j + 1])
+           {
+               temp = a[j];
+               a[j] = a[j+1];
+               a[j+1] = temp;
+               flag = 1;
+           }
+       }
+       if(flag == 0)
+       break;
+   }
+   
+   printf("\nAfter Sorting : ");
+   for(i = 0; i < 5; i++)
+   {
+       printf("%d ",a[i]);
+   }
+    
+    return 0;
+    
+}
+
+
