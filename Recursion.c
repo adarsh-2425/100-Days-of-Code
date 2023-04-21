45fa46055707e75012f81e79dc038642a98ca4e0
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-21 10:40:44 +0530

    Update Recursion.c

--- Recursion.c
+++ Recursion.c
@@ -17,3 +17,26 @@ int main()
     int n = 5;
     display(5);
 }
+
+
+//Sum of N natural numbers using recursion
+#include <stdio.h>
+
+int sum(int x)
+{
+    int s = 0;
+    
+    if(x == 0)
+        return x;
+    else
+    {
+        s = x + sum(x-1);
+        return s;
+    }
+}
+
+void main()
+{
+    int n = 3;
+    printf("%d", sum(n));
+}
