57804fad7038478f43e3051a18a662349c27fa0b
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-21 16:30:45 +0530

    Update Recursion.c

--- Recursion.c
+++ Recursion.c
@@ -110,3 +110,25 @@ int main()
 {
     print(10);
 }
+
+
+//Non - Tail Recursion
+
+#include <stdio.h>
+
+void print(int a)
+{
+    if(a < 1) 
+        return;
+    else
+    {
+        printf("%d ", a);
+        print(a/2);
+        printf("%d ", a);
+    }
+}
+
+int main()
+{
+    print(10);
+}
