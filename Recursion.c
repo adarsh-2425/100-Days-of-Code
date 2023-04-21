a2a993a9eebcbe1ad723b5804d20e501d411113f
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-21 11:13:42 +0530

    Update Recursion.c

--- Recursion.c
+++ Recursion.c
@@ -68,3 +68,24 @@ int fun2(int n)
     else
         return n * fun1(n-1);
 }
+
+
+//Factorial of a number using direct recursion
+#include <stdio.h>
+
+int fun1(int);
+
+
+void main()
+{
+    printf("%d", fun1(5));
+}
+
+int fun1(int n)
+{
+    if(n <= 1)
+        return 1;
+    else
+        return n * fun1(n-1);
+}
+
