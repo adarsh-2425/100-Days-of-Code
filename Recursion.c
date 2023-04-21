f284a578a851401118f28e5338c83e8987b54731
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-21 11:05:48 +0530

    Update Recursion.c

--- Recursion.c
+++ Recursion.c
@@ -40,3 +40,31 @@ void main()
     int n = 3;
     printf("%d", sum(n));
 }
+
+
+//Factorial of a number using indirect recursion
+#include <stdio.h>
+
+int fun1(int);
+int fun2(int);
+
+void main()
+{
+    printf("%d", fun1(5));
+}
+
+int fun1(int n)
+{
+    if(n <= 1)
+        return 1;
+    else
+        return n * fun2(n-1);
+}
+
+int fun2(int n)
+{
+    if(n <= 1)
+        return 1;
+    else
+        return n * fun1(n-1);
+}
