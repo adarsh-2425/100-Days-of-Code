e4f2bb1668bdc66448948389354689a0ada60b1d
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-22 17:46:45 +0530

    Update Structures.c

--- Structures.c
+++ Structures.c
@@ -16,3 +16,28 @@ int main()
     
     printf("%s", S1.name);
 }
+
+
+//Array of Structures
+
+#include <stdio.h>
+
+struct student
+{
+    int rollno;
+    char name[10];
+    float mark;
+};
+
+int main()
+{
+    struct student S[3];
+    
+    printf("Enter details of 3 students : ");
+    for(int i = 0; i < 3; i++)
+        scanf("%d %s %f", &S[i].rollno, S[i].name, &S[i].mark);
+    
+    printf("/n Details of 3 students are : ");   
+    for(int i = 0; i < 3; i++)
+        printf("\n  %d. %s - %f", S[i].rollno, S[i].name, S[i].mark);
+}
