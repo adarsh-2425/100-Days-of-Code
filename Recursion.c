d6ebe5720cff1cba88ba6b00fd4e048555330101
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-22 11:47:20 +0530

    Update Recursion.c

--- Recursion.c
+++ Recursion.c
@@ -132,3 +132,28 @@ int main()
 {
     print(10);
 }
+
+
+//Recursion Solved Problem (GATE 2016 Question)
+
+#include <stdio.h>
+
+void count(int n)
+{
+    static int d = 1;
+    
+    printf("%d ", n);
+    printf("%d ", d);
+    
+    d++;
+    
+    if(n >1) 
+        count(n-1);
+    
+    printf("%d ", d);
+}
+
+int main()
+{
+    count(3);
+}
