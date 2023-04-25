a1ca68036ee9f8b67f211cfacf197c18adf56687
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-25 08:46:43 +0530

    Update Arrays.c

--- Arrays.c
+++ Arrays.c
@@ -469,3 +469,41 @@ int main()
     return 0;
 }
 
+
+//remove Duplicate Element from a  sorted arry 
+
+#include <stdio.h>
+
+int duplicate_array(int [], int);
+
+int main()
+{
+    int arr[] = {1,2,3,4,4,4,5,6,6,6,7,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int i, j;
+    
+    j = duplicate_array(arr, n);
+    
+    for(i = 0; i < j; i++)
+        printf("%d ", arr[i]);
+}
+
+int duplicate_array(int arr[], int n)
+{
+    if(n == 0 || n == 1)
+        return n;
+    
+    int temp[n];
+    int i, j = 0;
+    
+    for(i = 0; i < n; i++)
+        if(arr[i] != arr[i+1])
+            temp[j++] = arr[i];
+            
+    temp[j++] = arr[n-1];
+    
+    for(i = 0; i < j; i++)
+        arr[i] = temp[i];
+        
+    return j;
+}
