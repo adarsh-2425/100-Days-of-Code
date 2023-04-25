cb4b633d6480982e72b6f2f94e420d441a7b5b7b
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-25 18:23:54 +0530

    Update Pointers.c

--- Pointers.c
+++ Pointers.c
@@ -197,3 +197,24 @@ int main()
    printf("%d", b);
 }
 
+//Pointer to a Function
+#include <stdio.h>
+
+int sum(int,int);
+
+int main()
+{
+   int s = 0;
+   int (*ptr)(int, int) = sum;
+   
+   s = ptr(2,5);
+   
+   printf("%d", s);
+   
+}
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
