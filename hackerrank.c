382ea8361ba98dee90b74122754951065391f3f9
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-26 17:00:47 +0530

    Update hackerrank.c

--- hackerrank.c
+++ hackerrank.c
@@ -19,3 +19,25 @@ int main() {
     
     return 0;
 }
+
+
+//addition using pointers
+#include <stdio.h>
+#include <stdlib.h>
+
+void update(int *a,int *b) {
+    int temp  = *a;
+    *a = *a + *b; 
+    *b = abs(temp - *b);  
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
