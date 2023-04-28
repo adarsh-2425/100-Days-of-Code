e519b64cb9c648e275bea7c4f27df26f05315632
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-28 08:56:00 +0530

    Update hackerrank.c

--- 1. C/hackerrank.c
+++ 1. C/hackerrank.c
@@ -41,3 +41,43 @@ int main() {
 
     return 0;
 }
+
+//Bitwise operators
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+void calculate_the_maximum(int n, int k) {
+  int maxa = 0, maxo = 0, maxx = 0;
+  int i, j;
+  
+  for(i = 1; i <= k; i++)
+    {
+        for(j = i+1; j <= n; j++)
+        {  
+            if(((i & j) > maxa) && (i & j) < k)
+                maxa = (i & j);
+                
+            if(((i | j) > maxo) && (i | j) < k)
+                maxo = (i | j);
+                
+            if(((i ^ j) > maxx) && (i ^ j) < k)
+                maxx = (i ^ j);
+        }
+    }
+    printf("%d", maxa);
+    printf("\n%d", maxo);
+    printf("\n%d", maxx);
+}
+
+int main() {
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+ 
+    return 0;
+}
