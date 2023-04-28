6f2c9e5047c44d457b2afc6ede49ab379bed3ed4
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-28 19:55:05 +0530

    Update Dynamic memory allocation.c

--- 1. C/Dynamic memory allocation.c
+++ 1. C/Dynamic memory allocation.c
@@ -18,3 +18,25 @@ int main()
         
     free(ptr);
 }
+
+
+//calloc
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, n = 5, *ptr;
+    
+    ptr = (int *) calloc(n, sizeof(int));
+    
+    printf("Enter values: \n");
+    for(i = 0; i < n; i++)
+        scanf("%d ", (ptr + i));
+        
+    for(i = 0; i < n; i++)
+        printf("%d ", *(ptr + i));
+        
+    free(ptr);
+}
