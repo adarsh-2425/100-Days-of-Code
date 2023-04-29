fd6fd776c1867f4b054ee017b88259c19e09bb31
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-04-29 19:45:03 +0530

    Update hackerrank.c

--- 1. C/hackerrank.c
+++ 1. C/hackerrank.c
@@ -161,3 +161,31 @@ int main() {
     free(s);
     return 0;
 }
+
+
+//digit frequency
+//https://www.hackerrank.com/challenges/frequency-of-digits-1/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char str[2000];
+    scanf("%s", str);
+
+    int count[10] = {0};
+    for(int i = 0; i < strlen(str); i++){
+        if(str[i] >= '0' && str[i] <= '9'){
+            count[str[i] - '0']++;
+        }
+    }
+    
+    for(int i = 0; i < 10; i++){
+        printf("%d ", count[i]);
+    }
+    
+    return 0;
+}
