b7722cbc02b9e4d04c468bff0f70f41a145479f5
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-05-02 09:43:24 +0530

    Update Variadic functions.c

--- 1. C/Variadic functions.c
+++ 1. C/Variadic functions.c
@@ -28,3 +28,43 @@ int main()
 
     return 0;
 }
+
+
+/******************************************************************************
+
+Write a function named print_strings() that takes a variable number of string arguments and prints 
+them to the console, separated by spaces. 
+The function should take at least one argument, and should print "no strings" if no arguments are provided.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void print_strings(int count, ...){
+    if (count ==  0) {
+        printf("no strings");
+        return;
+    }
+
+    va_list args;
+    va_start(args, count);
+    
+    const char *str = va_arg(args, const char*);
+    printf("%s", str);
+    
+    for(int i = 1; i < count; i++) {
+        str = va_arg(args, const char*);
+        printf(" %s", str);
+    }
+    va_end(args);
+    printf("\n");
+}
+
+int main()
+{
+    print_strings(2, "hello", "world");
+
+    return 0;
+}
