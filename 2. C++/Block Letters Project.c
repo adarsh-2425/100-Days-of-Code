42646f266ec2336dbd9eb6109ef94ba4b2bb9191
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-05-17 17:52:57 +0530

    Create Block Letters Project.c

---
+++ 2. C++/Block Letters Project.c
@@ -0,0 +1,13 @@
+//BLock Letter Project as part of Codecademy's C++ Learning Course
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "L    " "   O    " "L   "<< endl;
+    cout << "L    " " O   O  " "L   "<< endl;
+    cout << "L    " "O     O " "L   "<< endl;
+    cout << "L    " "O     O " "L   "<< endl;
+    cout << "L    " " O   O  " "L   "<< endl;
+    cout << "LLLLL" "   O    " "LLLLL"<< endl;
+    return 0;
+}
