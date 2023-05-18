3f2e1d5499462bbe456b98b4fb251b180d9f8c45
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-05-18 19:14:46 +0530

    Create Mile to Kilometre Converter.cpp

---
+++ 2. C++/Mile to Kilometre Converter.cpp
@@ -0,0 +1,23 @@
+//Create a program that asks for a distance in miles as input. The program will then output how much that distance is in kilometers
+
+#include <iostream>
+
+int main() {
+
+  double miles;
+  double kilometers;
+
+std::cout << "Mile to Kilometer Converter!" << "\n";
+
+std::cout << "\n";
+
+//getting distance in mile
+std::cout << "Enter Distance (in miles): ";
+
+std::cin >> miles;
+
+kilometers = miles * 1.609344;
+
+std::cout << "Distance in Kilometer is: " << kilometers << " kms" << "\n";
+
+}
